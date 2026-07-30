f8f327b3619564caf1ac83716b36666528f350ce
Thiago Windows <thiagocosta.a@hotmail.com>
2026-07-30 16:54:57 -0300

    removing unecessary space

--- app.py
+++ app.py
@@ -1,24 +1,18 @@
 from flask import Flask, render_template, jsonify
-
 import motor
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-
 @app.post("/motor/test")
 def test_motor():
-
     motor.test()
-
     return jsonify({
         "status": "ok"
     })
-
 
 if __name__ == "__main__":
     app.run(
